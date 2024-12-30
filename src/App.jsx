c2941f9b71e0c3968ffb63dbfed8477cc590d604
Josh <119835493+josh76543210@users.noreply.github.com>
2024-12-29 17:10:01 -0800

    Dynamically add project data to cards;

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Card from "./Card";
+import projectsData from "./assets/data/projects.json";
 
 import "./App.css";
 
@@ -8,12 +9,9 @@ function App() {
       <h1>My Frontend Mentor Projects</h1>
       <input type="text" placeholder="Search challenges..." />
       <div className="card-grid">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {projectsData.map((card) => (
+          <Card key={card.id} data={card} />
+        ))}
       </div>
       <footer>
         <p>Coded by Josh</p>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,30 @@
-import Image from "./assets/img/projects/3-column-preview-card-component.png";
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 import styles from "./Card.module.css";
 
-function Card() {
+function Card({ data }) {
+  // dynamically import screenshot
+  const [imgUrl, setImgUrl] = useState("");
+  import(data.screenshot).then((image) => setImgUrl(image));
+
   return (
     <div className={styles.card}>
-      <h2>My Project</h2>
-      <img src={Image} alt="" />
+      <h2>{data.title}</h2>
+      <img src={imgUrl.default} alt="Project Screenshot" />
       <div className={styles.links}>
-        <a href="#">Code</a>
-        <a href="#">Live Site</a>
+        <a href={data.repo} target="_blank">
+          Code
+        </a>
+        <a href={data.url} target="_blank">
+          Live Site
+        </a>
       </div>
     </div>
   );
 }
+Card.propTypes = {
+  data: PropTypes.object,
+};
 
 export default Card;
